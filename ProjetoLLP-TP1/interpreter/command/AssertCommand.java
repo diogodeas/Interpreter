76ed74d584940a76d2effa26d06e0fdf89ce7010
pedrorochacr <84788885+pedrorochacr@users.noreply.github.com>
2022-10-23 21:46:40 -0300

    Update AssertCommand.java

--- ProjetoLLP-TP1/interpreter/command/AssertCommand.java
+++ ProjetoLLP-TP1/interpreter/command/AssertCommand.java
@@ -2,13 +2,14 @@ package interpreter.command;
 import interpreter.expr.Expr;
 import interpreter.expr.ListItem;
 import interpreter.expr.SetExpr;
+import interpreter.util.Utils;
 import interpreter.value.BoolValue;
 import interpreter.value.Value;
-public class AssertCommand {
+public class AssertCommand extends Command{
     private Expr expr;
     private Expr msg;
 
-    public AssertCommand(int line, Expr expr, ListItem thenItem){
+    public AssertCommand(int line, Expr expr, Expr msg){
         super(line);
         this.expr = expr;
         this.msg = null;
@@ -17,6 +18,17 @@ public class AssertCommand {
         Value<?> v = expr.expr();
         BoolValue bv = (BoolValue) v;
         boolean b = bv.value();
+        if (!(v instanceof BoolValue)){
+            Utils.abort(super.getLine());
+        }
+        if(!b){
+            if(msg != null){
+                Value<?> mensagem = msg.expr();
+                System.out.println(mensagem);
+            }
+            else
+                System.out.println("Assert Invalid");
+        }
     }
 
 }
