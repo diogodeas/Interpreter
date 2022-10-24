3542a3422b824895543d36c444c12334678cf408
pedrorochacr <84788885+pedrorochacr@users.noreply.github.com>
2022-10-23 21:55:37 -0300

    Update SpreadListItem.java

--- ProjetoLLP-TP1/interpreter/expr/SpreadListItem.java
+++ ProjetoLLP-TP1/interpreter/expr/SpreadListItem.java
@@ -5,9 +5,10 @@ import java.util.List;
 import java.util.Map;
 
 import interpreter.util.Utils;
+import interpreter.value.ListValue;
 import interpreter.value.MapValue;
 import interpreter.value.Value;
-public class SpreadListItem {
+public class SpreadListItem extends ListItem{
 
     private Expr expr;
 
@@ -16,6 +17,14 @@ public class SpreadListItem {
         this.expr = expr;
     }
     public List<Value<?>> items() {
-
+        List<Value<?>>  spreadList = new ArrayList<>();
+        if(this.expr.expr() instanceof ListValue){
+            ListValue listv = (ListValue) this.expr.expr();
+            spreadList.addAll(listv.value());
+        }
+        else{
+            Utils.abort(super.getLine());
+        }
+        return spreadList;
     }
 }
