9f6652e219ee698bbed43678a5c6326b922d5589
pedrorochacr <84788885+pedrorochacr@users.noreply.github.com>
2022-10-23 21:47:37 -0300

    Update IfListItem.java

--- ProjetoLLP-TP1/interpreter/expr/IfListItem.java
+++ ProjetoLLP-TP1/interpreter/expr/IfListItem.java
@@ -1,24 +1,42 @@
 package interpreter.expr;
 
+import interpreter.util.Utils;
+import interpreter.value.BoolValue;
 import interpreter.value.Value;
 import interpreter.expr.Expr;
 import interpreter.expr.SetExpr;
 import interpreter.value.Value;
 
-public class IfListItem {
+import java.util.ArrayList;
+import java.util.List;
+
+public class IfListItem extends ListItem {
 
     private Expr expr;
     private ListItem thenItem;
     private ListItem elseItem;
 
-    public IfListItem(int line, Expr expr, ListItem thenItem){
+    public IfListItem(int line, Expr expr, ListItem thenItem, ListItem elseItem){
         super(line);
         this.expr = expr;
         this.thenItem = thenItem;
-        this.elseItem = null;
+        this.elseItem = elseItem;
     }
 
     public List<Value<?>> items(){
+        Value<?> value= expr.expr();
+        List<Value<?>> ifListV = new ArrayList<Value<?>>();
+        BoolValue bv = (BoolValue) value;
+        boolean b = bv.value();
+        if(!(value instanceof BoolValue)){
+            Utils.abort(super.getLine());
+        }
+        if(b)
+            ifListV.addAll(this.thenItem.items());
+        else
+            ifListV.addAll(this.elseItem.items());
+
+        return ifListV;
 
     }
 
