9445c2b70530482f63f33b0de0b8176277454db7
pedrorochacr <84788885+pedrorochacr@users.noreply.github.com>
2022-10-23 21:47:09 -0300

    Update ForListItem.java

--- ProjetoLLP-TP1/interpreter/expr/ForListItem.java
+++ ProjetoLLP-TP1/interpreter/expr/ForListItem.java
@@ -6,9 +6,10 @@ import java.util.Map;
 
 import interpreter.command.Command;
 import interpreter.util.Utils;
+import interpreter.value.ListValue;
 import interpreter.value.MapValue;
 import interpreter.value.Value;
-public class ForListItem {
+public class ForListItem extends  ListItem{
     private Variable variable;
     private Expr expr;
     private ListItem item;
@@ -17,12 +18,27 @@ public class ForListItem {
 
     public ForListItem(int line, Variable var, Expr expr, ListItem item){
         super(line);
-        super(var);
+       this.variable = var;
         this.expr = expr;
         this.item = item;
     }
 
     public List<Value<?>> items() {
+        List<Value<?>> forListItem = new ArrayList<Value<?>>();
+        Value<?> value = expr.expr();
 
+        if (!(value instanceof ListValue)){
+            Utils.abort(super.getLine());
+        }
+
+
+        ListValue listValue= (ListValue)   value;
+
+        for (Value<?> variavel : listValue.value()) {
+            variable.setValue(variavel);
+            forListItem.addAll(item.items());
+        }
+
+        return forListItem;
     }
 }
